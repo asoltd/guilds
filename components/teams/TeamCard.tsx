dc7f5e48abfe3be703500e2b872a001ed2894fd5
Prayash Shrestha <prayash.shrestha@wiseyak.com>
2022-10-22 20:13:08 +0545

    added mui components

--- components/teams/TeamCard.tsx
+++ components/teams/TeamCard.tsx
@@ -1,3 +1,12 @@
+import {
+  Avatar,
+  Card,
+  CardContent,
+  CardHeader,
+  Collapse,
+  Typography,
+} from "@mui/material"
+import { red } from "@mui/material/colors"
 import Image from "next/image"
 import Link from "next/link"
 import { Team } from "storage/team"
@@ -7,38 +16,42 @@ type CardProps = {
 }
 const TeamCard = ({ team }: CardProps): JSX.Element => {
   return (
-    <div>
-      <div>
-        <Image
-          src={`${team?.image}`}
-          alt={team.name.first}
-          width={500}
-          height={400}
-        />
-      </div>
-      <div>
-        <b> Name: </b>
-        {team.name.first} {team.name.second} {team.name.last}
-      </div>
-      <div>
-        <b>Title: </b>
-        {team?.title}
-      </div>
-      <div>
-        <b>Description:</b> {team?.description}
-      </div>
-      <div>
-        <b>Tags:</b> {team?.tags}
-      </div>
-      <Link
-        href={{
-          pathname: "/team",
-          query: { teamId: team.id },
-        }}
-      >
-        <button>view {team.name.first}</button>
-      </Link>
-    </div>
+    <Card sx={{ maxWidth: 345, backgroundColor: "#101828" }}>
+      <CardHeader
+        avatar={
+          <Avatar aria-label="recipe">
+            <Image
+              src={`${team?.image}`}
+              alt={team.name.first}
+              width={640}
+              height={680}
+            />
+          </Avatar>
+        }
+        title={
+          <Link
+            href={{
+              pathname: "/team",
+              query: { teamId: team.id },
+            }}
+          >
+            <Typography color={`#41764A`} sx={{ cursor: "pointer" }}>
+              {team.name.first} {team.name.second} {team.name.last}
+            </Typography>
+          </Link>
+        }
+        subheader={
+          <Link href="/">
+            <Typography color={`#667085`}>{team?.title}</Typography>
+          </Link>
+        }
+      />
+      <CardContent sx={{ hyphens: "auto" }}>
+        <Typography variant="body2" color="text.secondary" align="justify">
+          {team?.description}
+        </Typography>
+      </CardContent>
+    </Card>
   )
 }
 

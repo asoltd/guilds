11587dbdd0badcf9561d3cb543ffad57563c97d2
Jakub Frackowiak <xfragless@gmail.com>
2022-10-29 15:55:16 +0200

    add exports of social sign in components and add social icons

--- components/SocialSignIn/index.tsx
+++ components/SocialSignIn/index.tsx
@@ -1,2 +1,4 @@
 export { GoogleSignIn } from "./GoogleSignIn"
 export { TwitterSignIn } from "./TwitterSignIn"
+export { GithubSignIn } from "./GithubSignIn"
+export { FacebookSignIn } from "./FacebookSignIn"

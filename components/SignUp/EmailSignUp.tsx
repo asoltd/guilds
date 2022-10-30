a142b476f0874e858f53b57b558efdaba9e2f1a5
Jakub Frackowiak <xfragless@gmail.com>
2022-10-30 17:25:46 +0100

    add sign up email and phone tabs

--- components/SignUp/EmailSignUp.tsx
+++ components/SignUp/EmailSignUp.tsx
@@ -1,9 +1,12 @@
 import Image from "next/image"
 import * as Yup from "yup"
 import {
+  Box,
   Button,
   IconButton,
   InputAdornment,
+  Tab,
+  Tabs,
   TextField,
   Typography,
 } from "@mui/material"
@@ -13,16 +16,50 @@ import {
   sendEmailVerification,
 } from "firebase/auth"
 import { Form, Formik, FormikProps } from "formik"
-import { useState } from "react"
+import { SyntheticEvent, useState } from "react"
 import { Visibility, VisibilityOff } from "@mui/icons-material"
 import { useAuth } from "reactfire"
 
 interface FormValues {
   email: string
+  phone: string
   password: string
   confirmPassword: string
   remember: boolean
   name: string
+}
+
+interface TabPanelProps {
+  children?: React.ReactNode
+  index: number
+  value: number
+}
+
+function TabPanel(props: TabPanelProps) {
+  const { children, value, index, ...other } = props
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  )
+}
+
+function a11yProps(index: number) {
+  return {
+    id: `simple-tab-${index}`,
+    "aria-controls": `simple-tabpanel-${index}`,
+  }
 }
 
 const SignupSchema = Yup.object().shape({
@@ -41,6 +78,7 @@ const SignupSchema = Yup.object().shape({
     .oneOf([Yup.ref("password"), null], "Passwords must match!"),
   email: Yup.string().email("Invalid email!").required("Email required!"),
   name: Yup.string().required("Name required!"),
+  phone: Yup.string().required("Phone number required!"),
 })
 
 export function EmailSignUp() {
@@ -63,7 +101,10 @@ export function EmailSignUp() {
         })
       })
   }
-
+  const [value, setValue] = useState(0)
+  const handleTabsChange = (event: SyntheticEvent, newValue: number) => {
+    setValue(newValue)
+  }
   return (
     <Stack
       alignItems="center"
@@ -78,9 +119,29 @@ export function EmailSignUp() {
       <Typography variant="body1" sx={{ color: "text.secondary" }}>
         Start your 30-days trial.
       </Typography>
+      <Box width="100%">
+        <Tabs
+          variant="fullWidth"
+          value={value}
+          onChange={handleTabsChange}
+          sx={{ border: "10px" }}
+        >
+          <Tab
+            label="Email"
+            {...a11yProps(0)}
+            sx={{ textTransform: "none" }}
+          ></Tab>
+          <Tab
+            label="Phone"
+            {...a11yProps(1)}
+            sx={{ textTransform: "none" }}
+          ></Tab>
+        </Tabs>
+      </Box>
       <Formik
         initialValues={{
           email: "",
+          phone: "",
           password: "",
           confirmPassword: "",
           remember: false,
@@ -111,20 +172,38 @@ export function EmailSignUp() {
                   <Typography color="#ff0000">{errors.name}</Typography>
                 ) : null}
               </Stack>
-              <Stack>
-                <Typography variant="body1">Email*</Typography>
-                <TextField
-                  onChange={handleChange}
-                  value={values.email}
-                  size="small"
-                  type="email"
-                  name="email"
-                  placeholder="Email"
-                />
-                {errors.email && touched.email ? (
-                  <Typography color="#ff0000">{errors.email}</Typography>
-                ) : null}
-              </Stack>
+              <TabPanel value={value} index={0}>
+                <Stack>
+                  <Typography variant="body1">Email*</Typography>
+                  <TextField
+                    onChange={handleChange}
+                    value={values.email}
+                    size="small"
+                    type="email"
+                    name="email"
+                    placeholder="Email"
+                  />
+                  {errors.email && touched.email ? (
+                    <Typography color="#ff0000">{errors.email}</Typography>
+                  ) : null}
+                </Stack>
+              </TabPanel>
+              <TabPanel value={value} index={1}>
+                <Stack>
+                  <Typography variant="body1">Phone*</Typography>
+                  <TextField
+                    onChange={handleChange}
+                    value={values.phone}
+                    size="small"
+                    type="phone"
+                    name="phone"
+                    placeholder="Phone"
+                  />
+                  {errors.phone && touched.phone ? (
+                    <Typography color="#ff0000">{errors.phone}</Typography>
+                  ) : null}
+                </Stack>
+              </TabPanel>
               <Stack>
                 <Typography variant="body1">Password*</Typography>
                 <TextField
@@ -136,7 +215,7 @@ export function EmailSignUp() {
                   placeholder="••••••••"
                   InputProps={{
                     endAdornment: (
-                      <InputAdornment position="start">
+                      <InputAdornment position="end">
                         <IconButton
                           onClick={() => setShowPassword(!showPassword)}
                         >

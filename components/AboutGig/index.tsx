83b92f0fb082895663725b9116cbdbfb68a8aca1
Jakub Frackowiak <xfragless@gmail.com>
2022-11-04 12:16:16 +0100

    add about gig component

---
+++ components/AboutGig/index.tsx
@@ -0,0 +1,12 @@
+import { Typography } from "@mui/material"
+import { Stack } from "@mui/system"
+
+export function AboutGig() {
+  return (
+    <Stack>
+      <Stack>
+        <Typography variant="body1">Level 1 Quest</Typography>
+      </Stack>
+    </Stack>
+  )
+}

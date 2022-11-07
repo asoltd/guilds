7682ab865eb4af7dc392d5bc0bd1c4cbd48d08a3
Jakub Frackowiak <xfragless@gmail.com>
2022-11-07 19:29:26 +0100

    delete padding from page indexes

--- components/SignIn/index.tsx
+++ components/SignIn/index.tsx
@@ -5,19 +5,17 @@ import { providers } from "../SocialSignIn/providers"
 
 export function SignIn() {
   return (
-    <Box py="5rem">
-      <Stack m="auto" width="25rem" spacing="1rem">
-        <EmailSignIn />
-        <Stack direction="row">
-          {providers.map((provider) => (
-            <SocialSignIn key={provider.image} {...provider} />
-          ))}
-        </Stack>
-        <Stack direction="row" justifyContent="center" spacing={1} mt="2rem">
-          <Typography variant="body1">Don't have an account?</Typography>
-          <Link href="/signup">Sign up</Link>
-        </Stack>
+    <Stack m="auto" width="25rem" spacing="1rem">
+      <EmailSignIn />
+      <Stack direction="row">
+        {providers.map((provider) => (
+          <SocialSignIn key={provider.image} {...provider} />
+        ))}
       </Stack>
-    </Box>
+      <Stack direction="row" justifyContent="center" spacing={1} mt="2rem">
+        <Typography variant="body1">Don't have an account?</Typography>
+        <Link href="/signup">Sign up</Link>
+      </Stack>
+    </Stack>
   )
 }
--- components/SignUp/index.tsx
+++ components/SignUp/index.tsx
@@ -5,19 +5,17 @@ import { providers } from "../SocialSignIn/providers"
 
 export function SignUp() {
   return (
-    <Box py="5rem">
-      <Stack m="auto" width="25rem" spacing="1rem">
-        <EmailSignUp />
-        <Stack direction="row">
-          {providers.map((provider) => (
-            <SocialSignIn key={provider.image} {...provider} />
-          ))}
-        </Stack>
-        <Stack direction="row" justifyContent="center" spacing={1} mt="2rem">
-          <Typography variant="body1">Already have an account?</Typography>
-          <Link href="/login">Log in</Link>
-        </Stack>
+    <Stack m="auto" width="25rem" spacing="1rem">
+      <EmailSignUp />
+      <Stack direction="row">
+        {providers.map((provider) => (
+          <SocialSignIn key={provider.image} {...provider} />
+        ))}
       </Stack>
-    </Box>
+      <Stack direction="row" justifyContent="center" spacing={1} mt="2rem">
+        <Typography variant="body1">Already have an account?</Typography>
+        <Link href="/login">Log in</Link>
+      </Stack>
+    </Stack>
   )
 }

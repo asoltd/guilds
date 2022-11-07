bf35490f447cdf675022621c15fb4adced4debfb
Jakub Frackowiak <xfragless@gmail.com>
2022-11-07 19:31:28 +0100

    delete padding from about gig

--- components/AboutGig/AboutGig.tsx
+++ components/AboutGig/AboutGig.tsx
@@ -1,46 +1,44 @@
-import { Typography, Box } from "@mui/material"
+import { Typography } from "@mui/material"
 import { Stack } from "@mui/system"
 
 export function AboutGig({ quest }) {
   return (
-    <Box py="5rem">
-      <Stack spacing={4} alignItems="start">
-        <Stack spacing={3} p={{ xs: "1rem", sm: "1rem", md: "1rem" }}>
-          <Stack spacing={1}>
-            <Typography variant="body1" color=" primary.main" fontWeight="600">
-              Level {quest?.level} Quest
-            </Typography>
-            <Typography
-              variant="h3"
-              maxWidth={{
-                xs: "25rem",
-                sm: "35rem",
-                md: "100%",
-                lg: "100%",
-                xl: "100%",
-              }}
-            >
-              About {quest?.title}
-            </Typography>
-          </Stack>
-          <Typography variant="body1" color="text.secondary">
-            {quest?.summary}
+    <Stack spacing={4} alignItems="start">
+      <Stack spacing={3} p={{ xs: "1rem", sm: "1rem", md: "1rem" }}>
+        <Stack spacing={1}>
+          <Typography variant="body1" color=" primary.main" fontWeight="600">
+            Level {quest?.level} Quest
+          </Typography>
+          <Typography
+            variant="h3"
+            maxWidth={{
+              xs: "25rem",
+              sm: "35rem",
+              md: "100%",
+              lg: "100%",
+              xl: "100%",
+            }}
+          >
+            About {quest?.title}
           </Typography>
         </Stack>
-        <Typography
-          variant="body1"
-          color="text.secondary"
-          lineHeight="1.8rem"
-          p={{ xs: "1rem", sm: "1rem", md: "1rem" }}
-          sx={{
-            columnCount: { xs: 1, sm: 1, md: 2, lg: 2, xl: 2 },
-            columnGap: 4,
-            columnWidth: "50%",
-          }}
-        >
-          {quest?.description}
+        <Typography variant="body1" color="text.secondary">
+          {quest?.summary}
         </Typography>
       </Stack>
-    </Box>
+      <Typography
+        variant="body1"
+        color="text.secondary"
+        lineHeight="1.8rem"
+        p={{ xs: "1rem", sm: "1rem", md: "1rem" }}
+        sx={{
+          columnCount: { xs: 1, sm: 1, md: 2, lg: 2, xl: 2 },
+          columnGap: 4,
+          columnWidth: "50%",
+        }}
+      >
+        {quest?.description}
+      </Typography>
+    </Stack>
   )
 }

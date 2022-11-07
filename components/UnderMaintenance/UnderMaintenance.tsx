d33346131516201d5d1e3668be5dc2a8166093e6
Jakub Frackowiak <xfragless@gmail.com>
2022-11-07 17:02:49 +0100

    change sx styling to props styling

--- components/UnderMaintenance/UnderMaintenance.tsx
+++ components/UnderMaintenance/UnderMaintenance.tsx
@@ -6,25 +6,23 @@ import {
   Stack,
   TextField,
   Typography,
+  Box,
 } from "@mui/material"
-import { Box } from "@mui/system"
 
 export function UnderMaintenance() {
   return (
     <Stack
       p="5rem"
       direction="row"
+      alignItems="center"
+      justifyContent="center"
       spacing={{ xs: 0, sm: 0, md: 7, lg: 10, xl: 10 }}
-      sx={{
-        alignItems: "center",
-        justifyContent: "center",
-        flexDirection: {
-          xs: "column-reverse",
-          sm: "column-reverse",
-          md: "row",
-          lg: "row",
-          xl: "row",
-        },
+      flexDirection={{
+        xs: "column-reverse",
+        sm: "column-reverse",
+        md: "row",
+        lg: "row",
+        xl: "row",
       }}
     >
       <Stack spacing={3}>
@@ -34,11 +32,7 @@ export function UnderMaintenance() {
           </Typography>
           <Typography variant="h3">Under maintenance</Typography>
         </Stack>
-        <Typography
-          variant="body1"
-          color="text.secondary"
-          sx={{ maxWidth: "25rem" }}
-        >
+        <Typography variant="body1" color="text.secondary" maxWidth="25rem">
           Sorry, the page you are looking for doesn't exist or has been moved.
           Try searching our site:
         </Typography>
@@ -63,27 +57,23 @@ export function UnderMaintenance() {
         </Stack>
       </Stack>
       <Box
-        sx={{
-          display: {
-            xs: "block",
-            sm: "block",
-            md: "block",
-            lg: "none",
-            xl: "none",
-          },
+        display={{
+          xs: "block",
+          sm: "block",
+          md: "block",
+          lg: "none",
+          xl: "none",
         }}
       >
         <Image src="/404.svg" width={400} height={180} alt="404 error" />
       </Box>
       <Box
-        sx={{
-          display: {
-            xs: "none",
-            sm: "none",
-            md: "none",
-            lg: "block",
-            xl: "block",
-          },
+        display={{
+          xs: "none",
+          sm: "none",
+          md: "none",
+          lg: "block",
+          xl: "block",
         }}
       >
         <Image src="/404.svg" width={500} height={180} alt="404 error" />

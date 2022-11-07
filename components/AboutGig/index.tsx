ee35384f10b4eb536b90a906e342fe0be3cc8324
Jakub Frackowiak <xfragless@gmail.com>
2022-11-07 16:45:28 +0100

    fix requested changes

--- components/AboutGig/index.tsx
+++ components/AboutGig/index.tsx
@@ -3,25 +3,16 @@ import { Stack } from "@mui/system"
 
 export function AboutGig({ quest }) {
   return (
-    <Stack
-      spacing={4}
-      sx={{
-        alignItems: "start",
-      }}
-      mt={10}
-    >
+    <Stack spacing={4} alignItems="start" mt={10}>
       <Stack
         spacing={3}
         sx={{
-          p: { xs: "1rem", sm: "1rem", md: "1rem", lg: "0rem", xl: "0rem" },
+          p: { xs: "1rem", sm: "1rem", md: "1rem" },
         }}
       >
         <Stack spacing={1}>
-          <Typography
-            variant="body1"
-            sx={{ color: "primary.main", fontWeight: "600" }}
-          >
-            Level {quest.level} Quest
+          <Typography variant="body1" color=" primary.main" fontWeight="600">
+            Level {quest?.level} Quest
           </Typography>
           <Typography
             variant="h3"
@@ -38,20 +29,19 @@ export function AboutGig({ quest }) {
             About {quest?.title}
           </Typography>
         </Stack>
-        <Typography variant="body1" sx={{ color: "text.secondary" }}>
-          {quest.summary}.
+        <Typography variant="body1" color="text.secondary">
+          {quest?.summary}
         </Typography>
       </Stack>
       <Typography
         variant="body1"
+        color="text.secondary"
+        lineHeight="1.8rem"
         sx={{
           columnCount: { xs: 1, sm: 1, md: 2, lg: 2, xl: 2 },
-          lineHeight: "1.8rem",
           columnGap: 4,
           columnWidth: "50%",
-          height: "900px",
-          color: "text.secondary",
-          p: { xs: "1rem", sm: "1rem", md: "1rem", lg: "0rem", xl: "0rem" },
+          p: { xs: "1rem", sm: "1rem", md: "1rem" },
         }}
       >
         {quest?.description}

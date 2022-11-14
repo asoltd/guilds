63deb73a887cd9f21d4efad6efe02aa368d1c87d
Jakub Frackowiak <xfragless@gmail.com>
2022-11-14 19:18:38 +0100

    fix requested changes in AboutGig

--- components/AboutGig/AboutGig.tsx
+++ components/AboutGig/AboutGig.tsx
@@ -1,29 +1,27 @@
-import { Typography, Stack } from "@mui/material"
+import { Typography, Stack, useMediaQuery } from "@mui/material"
 import { Quest } from "storage/quest"
+import { useTheme } from "@mui/material/styles"
 
 interface QuestProps {
   quest: Quest
 }
 
 export function AboutGig({ quest }: QuestProps) {
+  const theme = useTheme()
+  const isMedium = useMediaQuery(theme.breakpoints.down("lg"))
+
   return (
     <Stack spacing={4} alignItems="start">
-      <Stack spacing={3} p={{ xs: "1rem", sm: "1rem", md: "1rem" }}>
+      <Stack spacing={3} p={isMedium ? "1.5rem" : "0"}>
         <Stack spacing={1}>
           <Typography variant="body1" color=" primary.main" fontWeight="600">
             Level {quest.level} Quest
           </Typography>
-          <Typography
-            variant="h3"
-            maxWidth={{
-              xs: "25rem",
-              sm: "35rem",
-            }}
-          >
+          <Typography variant="h3" maxWidth="sm">
             About {quest.title}
           </Typography>
         </Stack>
-        <Typography variant="body1" color="text.secondary">
+        <Typography variant="body1" color="text.secondary" maxWidth="sm">
           {quest.summary}
         </Typography>
       </Stack>
@@ -31,7 +29,7 @@ export function AboutGig({ quest }: QuestProps) {
         variant="body1"
         color="text.secondary"
         lineHeight="1.8rem"
-        p={{ xs: "1rem", sm: "1rem", md: "1rem" }}
+        p={isMedium ? "1.5rem" : "0"}
         sx={{
           columnCount: { xs: 1, sm: 1, md: 2, lg: 2, xl: 2 },
           columnGap: 4,

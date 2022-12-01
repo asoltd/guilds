e88f2e5d54b2aac81a4869521416a5221f7a8c39
Oliwier Ostrowski <oliwier.ostro@gmail.com>
2022-12-01 20:12:08 +0100

    Adjust green button color to match design

--- components/ReusableComponents/buttons.tsx
+++ components/ReusableComponents/buttons.tsx
@@ -2,13 +2,21 @@ import { Button, Typography } from "@mui/material"
 
 export function GreenButton({ children }) {
   return (
-    <Button variant="contained" sx={{ height: "3rem", borderRadius: "0.6rem" }}>
+    <Button
+      variant="contained"
+      sx={{
+        height: "3rem",
+        borderRadius: "0.6rem",
+        bgcolor: "#50915B",
+      }}
+    >
       <Typography
         textAlign="center"
         textTransform="none"
         px="0.6rem"
         fontWeight={500}
         variant="body1"
+        sx={{ textDecoration: "none" }}
       >
         {children}
       </Typography>

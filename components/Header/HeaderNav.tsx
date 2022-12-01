fcbb89563c3584f7184f766a0a043085df38280e
Oliwier Ostrowski <oliwier.ostro@gmail.com>
2022-12-01 21:07:41 +0100

    Add arrow image, update navigation object

--- components/Header/HeaderNav.tsx
+++ components/Header/HeaderNav.tsx
@@ -1,4 +1,5 @@
 import { MenuItem, Stack, Typography } from "@mui/material"
+import Image from "next/image"
 import Link from "next/link"
 import { NavigationColumn } from "../../navigation"
 
@@ -15,18 +16,28 @@ export const HeaderNav = ({ pages }: HeaderNavProps) => {
             href={page.href}
             style={{ textDecoration: "none", color: "inherit" }}
           >
-            <Typography
-              textAlign="center"
-              fontWeight={500}
-              variant="body2"
-              color={(theme) =>
-                theme.palette.mode == "light"
-                  ? theme.palette.grey[600]
-                  : theme.palette.grey[300]
-              }
-            >
-              {page.label}
-            </Typography>
+            <Stack direction="row" alignItems="center" spacing={1}>
+              <Typography
+                textAlign="center"
+                fontWeight={500}
+                variant="body2"
+                color={(theme) =>
+                  theme.palette.mode == "light"
+                    ? theme.palette.grey[600]
+                    : theme.palette.grey[300]
+                }
+              >
+                {page.label}
+              </Typography>
+              {page.navigations ? (
+                <Image
+                  src="/arrow-up.svg"
+                  width={10}
+                  height={10}
+                  alt="arrow up"
+                />
+              ) : null}
+            </Stack>
           </Link>
         </MenuItem>
       ))}

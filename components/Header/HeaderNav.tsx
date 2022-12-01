4d0b0cebc7908d1d7b0d0bbf5ec38abbb6b2dddc
Oliwier Ostrowski <oliwier.ostro@gmail.com>
2022-12-01 23:16:28 +0100

    Add ability to open and close the about us dropdown menu

--- components/Header/HeaderNav.tsx
+++ components/Header/HeaderNav.tsx
@@ -1,6 +1,8 @@
 import { MenuItem, Stack, Typography } from "@mui/material"
+import { AboutUsDropdown } from "components/AboutUsDropdown"
 import Image from "next/image"
 import Link from "next/link"
+import { useState } from "react"
 import { NavigationColumn } from "../../navigation"
 
 interface HeaderNavProps {
@@ -8,15 +10,62 @@ interface HeaderNavProps {
 }
 
 export const HeaderNav = ({ pages }: HeaderNavProps) => {
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null)
+  const open = Boolean(anchorEl)
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setAnchorEl(event.currentTarget)
+  }
+  const handleClose = () => {
+    setAnchorEl(null)
+  }
   return (
     <Stack direction="row">
       {pages.navigationItem.map((page) => (
         <MenuItem key={page.href}>
-          <Link
-            href={page.href}
-            style={{ textDecoration: "none", color: "inherit" }}
-          >
-            <Stack direction="row" alignItems="center" spacing={1}>
+          {page.navigations ? (
+            <Stack>
+              <button
+                aria-controls={open ? "basic-menu" : undefined}
+                aria-haspopup="true"
+                aria-expanded={open ? "true" : undefined}
+                onClick={handleClick}
+                style={{
+                  all: "unset",
+                }}
+              >
+                <Stack direction="row" alignItems="center" spacing={1}>
+                  <Typography
+                    textAlign="center"
+                    fontWeight={500}
+                    variant="body2"
+                    color={(theme) =>
+                      theme.palette.mode == "light"
+                        ? theme.palette.grey[600]
+                        : theme.palette.grey[300]
+                    }
+                  >
+                    {page.label}
+                  </Typography>
+                  <Image
+                    src="/arrow-up.svg"
+                    width={10}
+                    height={10}
+                    alt="arrow up"
+                  />
+                </Stack>
+              </button>
+              <AboutUsDropdown
+                handleClose={handleClose}
+                open={open}
+                anchorEl={anchorEl}
+                navigations={page.navigations}
+              />
+            </Stack>
+          ) : (
+            <Link
+              href={page.href}
+              style={{ textDecoration: "none", color: "inherit" }}
+            >
               <Typography
                 textAlign="center"
                 fontWeight={500}
@@ -29,16 +78,8 @@ export const HeaderNav = ({ pages }: HeaderNavProps) => {
               >
                 {page.label}
               </Typography>
-              {page.navigations ? (
-                <Image
-                  src="/arrow-up.svg"
-                  width={10}
-                  height={10}
-                  alt="arrow up"
-                />
-              ) : null}
-            </Stack>
-          </Link>
+            </Link>
+          )}
         </MenuItem>
       ))}
     </Stack>

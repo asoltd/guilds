6c92d4dc4fba3d3049ef9e11f13f90db8f40ebe3
Oliwier Ostrowski <oliwier.ostro@gmail.com>
2022-12-10 17:49:31 +0100

    Move buttons to separate directiories, annotate children type

--- components/WhiteButton/index.tsx
+++ components/WhiteButton/index.tsx
@@ -1,30 +1,10 @@
 import { Button, Typography } from "@mui/material"
 
-export function GreenButton({ children }) {
-  return (
-    <Button
-      variant="contained"
-      sx={{
-        height: "3rem",
-        borderRadius: "0.6rem",
-        bgcolor: "#50915B",
-      }}
-    >
-      <Typography
-        textAlign="center"
-        textTransform="none"
-        px="0.6rem"
-        fontWeight={500}
-        variant="body1"
-        sx={{ textDecoration: "none" }}
-      >
-        {children}
-      </Typography>
-    </Button>
-  )
+interface ButtonProps {
+  children: JSX.Element
 }
 
-export function WhiteButton({ children }) {
+export function WhiteButton({ children }: ButtonProps) {
   return (
     <Button
       variant="outlined"

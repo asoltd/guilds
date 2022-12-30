fc11115337d08e072acbeefeddbc32f898228116
Piotr Ostrowski <piotrostr@google.com>
2022-12-30 22:05:41 +0100

    add snackbars, fix the SocialSignIn

--- components/SocialSignIn/index.tsx
+++ components/SocialSignIn/index.tsx
@@ -1,14 +1,36 @@
 import Image from "next/image"
-import { Button } from "@mui/material"
+import { Button, Snackbar, Alert, AlertColor } from "@mui/material"
 import { Stack } from "@mui/system"
 import { signInWithPopup, AuthProvider } from "firebase/auth"
 import { useAuth } from "reactfire"
+import { useState } from "react"
 
 export function SocialSignIn({ provider, image, bgcolor }) {
   const auth = useAuth()
+  const [severity, setSeverity] = useState<AlertColor>("info")
+  const [message, setMessage] = useState("")
+  const [open, setOpen] = useState(false)
 
-  const SignInWithGoogle = async (provider: AuthProvider) => {
-    const result = await signInWithPopup(auth, provider)
+  const signInWithProvider = async (provider: AuthProvider) => {
+    try {
+      const result = await signInWithPopup(auth, provider)
+
+      if (result.operationType === "signIn") {
+        setMessage("Signed in successfully");
+      } else if (result.operationType === "link") {
+        setMessage("Linked successfully");
+      } else if (result.operationType === "reauthenticate") {
+        setMessage("Reauthenticated successfully");
+      }
+
+      setSeverity("success");
+      setOpen(true)
+
+    } catch (error) {
+      setSeverity("error")
+      setMessage(error.message)
+      setOpen(true)
+    }
   }
 
   return (
@@ -22,12 +44,21 @@ export function SocialSignIn({ provider, image, bgcolor }) {
             bgcolor: bgcolor,
           },
         }}
-        onClick={() => SignInWithGoogle(provider)}
+        onClick={() => signInWithProvider(provider)}
       >
         <Stack direction="row" spacing={2}>
           <Image width={24} height={24} src={image} alt="Social icon" />
         </Stack>
       </Button>
+      <Snackbar
+        open={open}
+        autoHideDuration={6000}
+        onClose={() => setOpen(false)}
+      >
+        <Alert severity={severity}>
+          {message}
+        </Alert>
+      </Snackbar>
     </Stack>
   )
 }
